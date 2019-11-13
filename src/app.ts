b940cf94670052b6c909dbd33ce61984a0ba8526
Rob Lankey <roblankey@quickenloans.com>
2019-11-13 13:08:14 -0500

    basic ts express with health and version

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
+
+import * as healthController from './controllers/health';
+import * as versionController from './controllers/version';
+
+const app: express.Application = express();
+
+// express settings
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/health', healthController.get);
+app.get('/version', versionController.get);
+
+export = app;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import * as errorHandler from 'errorhandler';
+
+import * as app from './app';
+
+app.use(errorHandler());
+
+// start express
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    '  App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  );
+  console.log('  Press CTRL-C to stop\n');
+});
+
+export = server;
